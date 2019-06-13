8bcbec0ab06a4ea4716a14644a3bb377b24c33a6
Mahi <47994802+Mahisaurus-Rex@users.noreply.github.com>
2019-06-13 19:03:31 -0400

    added base php stuff

--- login.php
+++ login.php
@@ -1,6 +1,13 @@
 <?php
+$username=$_POST['username']; //gets all info from index.html
+$email=$_POST['email'];
+$password=$_POST['password'];
+
+
+
 $con=mysqli_connect("localhost", "root","","login");//mysqli(""localhost", "username","password","database name");
-$result=mysql_query($con,"select ")
+$result=mysql_query($con,"SELECT * FROM `login_info` WHERE `username` = '$username' && `email = '$email' && `password` = '$password'")
+
 
 
 ?>
